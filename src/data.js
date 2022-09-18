c25c924c986f19a238b2c52236193e9667ae47b1
mamta <mamta@mamtas-MacBook-Air.local>
2022-09-18 22:39:11 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: "Zwitch.io",
+    subtitle: "Golang | Gin | PostgreSQL",
+    description:
+      "Working as a Lead Software Engineer for zwitch.io, a no-code fintech platform which helps our integrating partners create their own fintech service by embedding banking features like savings or current accounts, credit or debit cards, payments and lending.",
+    image: "./project-1.gif",
+    link: "https://www.zwitch.io/",
+  },
+  {
+    title: "COFTA",
+    subtitle: "Java | Spring Boot | MySQL",
+    description:
+      "Worked as a software developer in building COFTA (COgnitive Functional Testing Application) which is an automated test case execution tool that processes excel sheets via an NLU engine and executes the same by generating an automation test script in Selenium and an HTML report.",
+    image: "./project-2.gif",
+    link: "https://www2.deloitte.com/us/en/pages/consulting/solutions/quality-engineering-services.html",
+  },
+];
+
+export const skills = [
+  "Golang",
+  "Java",
+  "Python",
+  "GIT",
+  "PostgreSQL",
+  "MySQL",
+  "AWS",
+  "GCP",
+
+];
